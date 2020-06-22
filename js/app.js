ecb552a218466721024088be631d088b5ecb98d2
的場郁美 <>
2020-06-23 00:12:31 +0900

    html, css作成

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 
 
+// ハンバーガーメニュー
+$(function () {
+  $('.nav_toggle').on('click', function () {
+    $('.nav_toggle, .nav').toggleClass('show');
+  });
+});
+
 // トップ動画再生、停止
-let movie = document.getElementById('movie');
+let movie = document.getElementById('click');
 movie.addEventListener('click', function () {
 
   const video = document.querySelector("#movie");       // <audio>
