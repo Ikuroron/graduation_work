c4e845130bab46506fe8ca966d9d467c976e4536
的場郁美 <>
2020-06-23 21:04:13 +0900

    レスポンシブ前完成

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@
 
 // ハンバーガーメニュー
 $(function () {
+  
   $('.nav_toggle').on('click', function () {
     $('.nav_toggle, .nav').toggleClass('show');
   });
+
 });
 
 // トップ動画再生、停止
@@ -19,4 +21,31 @@ movie.addEventListener('click', function () {
     video.play();
   }
 
+})
+
+
+// 矢印スクロール
+$(function(){
+    
+    
+  // 矢印がクリックされたら、スムーススクロール
+  $('.arrow-up').click(() => {
+      $('body, html').animate({ scrollTop: 0,}, 600);
+  })
+
+  
+  // 画面がスクロールされたら
+  $(window).scroll(function () {
+      
+      // 画面のスクロールされた量を取得
+      let scrollTop = $(document).scrollTop();
+      // スクロールの量が300以上なら表示、それ以外は非表示
+      if (scrollTop >= 300) {
+          $('.arrow-up').fadeIn();
+      } else {
+          $('.arrow-up').fadeOut();
+      }
+
+
+  });
 })
